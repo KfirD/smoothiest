476fef13818ed6b0f5b3e082abb0f2cdc3c0eae5
Keegan Papakipos <keegan-git@papakipos.com>
2017-07-29 13:11:59 -0700

    more work on main page

--- Smoothiest/Smoothiest/FirstViewController.swift
+++ Smoothiest/Smoothiest/FirstViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var strawberrySwitch: UISwitch!
+    @IBOutlet weak var mangoSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func gimmeASmoothiePressed(_ sender: Any) {
+        
     }
-
 
 }
 
