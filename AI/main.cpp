0c531776abb8f2bea1faf6a0311c38badf7e1e07
Kfir  Dolev <bazilikan@gmail.com>
2017-07-29 15:54:59 -0700

    added population.h/.cpp

--- AI/main.cpp
+++ AI/main.cpp
@@ -11,17 +11,12 @@ int main() {
    Neurons inputs = {Neuron(0), Neuron(2)};
    Neurons outputs = {Neuron(1), Neuron(4)};
 
+   Neuron keiths_only_neuron(1);
+   cout << keiths_only_neuron.get_id();
+
    Network n(inputs, outputs);
    n.connect(inputs.front(), outputs.front(), 0);
    cout << n << "\n";
-
-   /*Connection edge(one, two, 0.5);
-
-   cout << edge.get_id() << "\n";
-   cout << edge.get_weight() << "\n";
-   cout << edge.get_src().get_id() << "\n";
-   cout << edge.get_dst().get_id() << "\n";*/
-
 
    return 0;
 }
